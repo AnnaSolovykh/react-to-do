1a9de68ea7f85ed98b93474b8afc8be61a0414fe
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-05-02 17:10:21 +0300

    asynchronous data and conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React, { useEffect, useState } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-const useSemiPersistentState = () => {
-
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList"))|| []);
-
-  useEffect(()=>{
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList])
-  
-  return [todoList, setTodoList]
-}
 
 function App() {
-const [todoList, setTodoList] = useSemiPersistentState();
+
+const [todoList, setTodoList] = useState([]);
+const [isLoading, setIsLoading] = useState(true)
+
+useState(() => {
+  new Promise (resolve => {
+    setTimeout(
+      () => resolve(
+        { data: {todoList:
+        JSON.parse(localStorage.getItem("savedTodoList"))|| []
+      }}
+      ),
+      2000
+    )
+  })
+  .then (result => {
+    setTodoList(result.data.todoList);
+    setIsLoading(false);
+  })
+}, []);
+
+useEffect(()=> {
+  
+  if (isLoading === false) {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }
+}, [todoList, isLoading]);
+
 
 const removeTodo = (id) => {
   console.log(todoList)
@@ -26,31 +42,27 @@ const removeTodo = (id) => {
 };
 
 const addTodo = (newTodo) => {
-  setTodoList(
-    [...todoList, newTodo]
-  );
-  //console.log(newTodo.id)
-
-  // const newToDoList = [];
-  // for (let i = 0; i < todoList.length; i++) {
-  //   newToDoList.push(todoList[i])
-  // };
-  // newToDoList.push(newTodo);
-  // setTodoList(newToDoList);
-  
-  if (newTodo.title === "") {
+  if (newTodo.title === "" || newTodo.title === " ") {
     alert("Write something!")
+  } else {
+    setTodoList(
+      [...todoList, newTodo]
+    );
   }
-}
+};
 
     return (
       <div style={{ 
           display: "flex", 
           flexDirection: "column",  
           alignItems: "center" }}>
-        <h1>What are you going to today?</h1>
+        <h1>What are you going to do today?</h1>
         <AddTodoForm onAddTodo={addTodo}/>
-        <TodoList todoList={todoList} onRemoveItem={removeTodo}/>
+        {isLoading ? (
+          <p>Loading...</p>
+        ): (
+            <TodoList todoList={todoList} onRemoveItem={removeTodo}/>
+        )}
       </div>
     );
 }
